a7e10cdafd8175ec62a7218d18d83bd14e652eb0
jasur911 <jasur150691@yandex.ru>
2020-04-18 04:47:54 +0000

    py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def wsgi_application(environ, start_response):
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plan')
+	]
+	body = "Hello, world!"
+	start_response(status, headers)
+	return [ body ]
